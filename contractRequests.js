8eb81b321f93b119f9eab6151f7323e0802c8ac7
Philip  Chwistek <chwistek.philip@gmail.com>
2017-11-27 14:07:36 -0500

    refactored server for the better

---
+++ contractRequests.js
@@ -0,0 +1,117 @@
+//imports for connecting to contract, https://ethereum.stackexchange.com/questions/24684/truffle-and-node-js
+var Web3 = require('web3'),
+contract = require('truffle-contract'),
+path = require('path'),
+WagerContractJSON = require(path.join(__dirname, 'build/contracts/Wager.json'));
+
+var provider = new Web3.providers.HttpProvider("http://localhost:9545");
+var account_one = "0x627306090abab3a6e1400e9345bc60c78a8bef57"; //account that deploys contract in Truffle
+
+var WagerContract = contract(WagerContractJSON);
+WagerContract.setProvider(provider);
+
+var wager = {
+
+	wagerInstance: {},
+
+	canDistribute: false,
+
+	requestNewWager: function(){
+
+        var tempMatch = {
+          wagerEventId: 0,
+          gameId: '28125',
+          takingBets: false,
+          moneyPoolFor: 0,
+          moneyPoolAgainst: 0
+        };
+
+        WagerContract.deployed().then(function(instance) {
+          this.wagerInstance = instance;
+          return this.wagerInstance.newWagerEvent(0, {from: account_one});
+        }).then(function(result) {
+
+          console.log("started taking bets");
+          for (var i = 0; i < result.logs.length; i++) {
+            var log = result.logs[i];
+
+            if (log.event == "TakingBets") {
+              console.log("The event id: " + log.args._wagerEventId.c[0]);
+              tempMatch.wagerEventId = log.args._wagerEventId.c[0];
+              tempMatch.takingBets = log.args._isBetting;
+              break;
+            }
+          }
+
+        }).catch(function (e){
+          console.log(e);
+        }); 
+
+        return tempMatch;
+    },
+
+    requestStopBetting: function(tempMatch){
+
+    	wagerInstance.stopBetting(tempMatch.wagerEventId, {from: account_one}).then(function(result){
+          console.log("stopped taking bets");
+          for (var i = 0; i < result.logs.length; i++) {
+            var log = result.logs[i];
+
+            if (log.event == 'Distributable') {
+              tempMatch.takingBets = log.args._isBetting;
+              this.canDistribute = log.args._enoughToDistribute;
+            }
+        }
+
+        }).catch(function (e){
+            console.log(e);
+        }); 
+
+        return tempMatch;
+    },
+
+    requestDistributeMoney: function(tempMatch){
+
+      var redWinner = false; //should be taken from gameData, but the test API call doesn't have it
+      wagerInstance.distributeMoney(tempMatch.wagerEventId, redWinner, {from: account_one}).then(function(result){
+        console.log("distributed winnings");
+      }).catch(function (e){
+        console.log(e);
+      }); 
+
+    },
+
+    requestReturnMoney: function(tempMatch){
+
+	  wagerInstance.returnMoney(tempMatch.wagerEventId, {from: account_one}).then(function(result){
+        console.log("returned bets");
+      }).catch(function (e){
+        console.log(e);
+      }); 
+
+    },
+
+    getUpdateMoneyPool: function(tempMatch){
+
+      wagerInstance.getMoneyPoolAgainst.call(tempMatch.wagerEventId, {from: account_one}).then(function(result){
+        console.log("Money pool for blue win: " + result.toNumber());
+        tempMatch.moneyPoolAgainst = result.toNumber();
+      }).catch(function (e){
+        console.log(e);
+      }); 
+
+      wagerInstance.getMoneyPoolFor.call(tempMatch.wagerEventId, {from: account_one}).then(function(result){
+        console.log("Money pool for red win: " + result.toNumber());
+        tempMatch.moneyPoolFor = result.toNumber();
+      }).catch(function (e){
+        console.log(e);
+      }); 
+
+      return tempMatch;
+    }
+}
+
+module.exports = wager;
+
+
+
--- server.js
+++ server.js
@@ -7,18 +7,8 @@ var clairvoyanceRouter = require('./expressRoutes/clairvoyanceRouter.js');
 const keys = require('./pandaScoreKeys');
 var ClientSocket = require('ws');
 var port = 3000;
+var wager = require('./contractRequests');
 
-//imports for connecting to contract, https://ethereum.stackexchange.com/questions/24684/truffle-and-node-js
-var Web3              = require('web3'),
-    contract          = require('truffle-contract'),
-    path              = require('path')
-    WagerContractJSON = require(path.join(__dirname, 'build/contracts/Wager.json'));
-
-var provider = new Web3.providers.HttpProvider("http://localhost:9545");
-var account_one = "0x627306090abab3a6e1400e9345bc60c78a8bef57"; //account that deploys contract in Truffle
-
-var WagerContract = contract(WagerContractJSON);
-WagerContract.setProvider(provider);
 
 //====================== REST Routes ==============================//
 app.use('/', clairvoyanceRouter); //for REST
@@ -36,10 +26,9 @@ app.get('/', function(req, res, next){
 
 //============================ Websocket begins ===============================
 
-var currentMatch = {};
+var currentMatch = null;
 var pandaSocket = new ClientSocket('wss://live.test.pandascore.co/matches/28125?token=' + keys.token);
 var takingMessages = true;
-var wager;
 
 pandaSocket.onmessage = function(event) {
 
@@ -49,106 +38,33 @@ pandaSocket.onmessage = function(event) {
 
       takingMessages = false;
 
-      var temp = JSON.parse(event.data);
+      var tempData = JSON.parse(event.data);
 
-      //currentMatch doesn't exist
-      if(currentMatch.wagerEventId == null && temp.type != 'hello'){
+      if(currentMatch == null){
 
-        var tempMatch = {
-          wagerEventId: 0,
-          gameId: '28125',
-          takingBets: false,
-          moneyPoolFor: 0,
-          moneyPoolAgainst: 0,
-          gameData: temp
-        };
+        if(tempData.type != 'hello'){
 
-        WagerContract.deployed().then(function(instance) {
-          wager = instance;
-          return wager.newWagerEvent(0, {from: account_one});
-        }).then(function(result) {
+          currentMatch = wager.requestNewWager(currentMatch);
+          currentMatch.gameData = tempData;
 
-          console.log("started taking bets");
-
-          for (var i = 0; i < result.logs.length; i++) {
-            var log = result.logs[i];
-
-            if (log.event == "TakingBets") {
-              console.log("The event id: " + log.args._wagerEventId.c[0]);
-              tempMatch.wagerEventId = log.args._wagerEventId.c[0];
-              tempMatch.takingBets = log.args._isBetting;
-              break;
-            }
-          }
-
-        }).catch(function (e){
-          console.log(e);
-        }) 
-
-        currentMatch = tempMatch;
+        }
 
       } else {
 
+        currentMatch.gameData = tempData;
 
-        if (currentMatch.gameData.game.finished == true){
+        if(currentMatch.gameData.game.finished){
 
-          var distribute = false;
-          
-          //stop from allowing bets
-          wager.stopBetting(currentMatch.wagerEventId, {from: account_one}).then(function(result){
-            console.log("ended taking bets");
-            for (var i = 0; i < result.logs.length; i++) {
-              var log = result.logs[i];
+          currentMatch = wager.requestStopBetting(currentMatch);
+          wager.canDistribute ? wager.requestDistributeMoney(currentMatch): wager.requestReturnMoney(currentMatch);
 
-              if (log.event == 'Distributable') {
-                currentMatch.takingBets = log.args._isBetting;
-                distribute = log.args._enoughToDistribute;
-              }
-            }
-
-          }).catch(function (e){
-            console.log(e);
-          }); 
-
-          if(distribute){
-
-            var redWinner = false; //should be taken from gameData, but the test API call doesn't have it
-
-            wager.distributeMoney(currentMatch.wagerEventId, redWinner, {from: account_one}).then(function(result){
-              console.log("distributed winnings");
-            }).catch(function (e){
-              console.log(e);
-            }); 
-
-          } else {
-
-            wager.returnMoney(currentMatch.wagerEventId, {from: account_one}).then(function(result){
-              console.log("returned bets");
-            }).catch(function (e){
-              console.log(e);
-            }); 
-
-            currentMatch = {}; //reset currentMatch
-          } 
+          currentMatch = {}; // reset game
 
         } else {
 
-          //update the bettings items
-          wager.getMoneyPoolAgainst.call(currentMatch.wagerEventId, {from: account_one}).then(function(result){
-            console.log("Money pool against red win: " + result.toNumber());
-            currentMatch.moneyPoolAgainst = result.toNumber();
-          }).catch(function (e){
-            console.log(e);
-          }); 
-
-          wager.getMoneyPoolFor.call(currentMatch.wagerEventId, {from: account_one}).then(function(result){
-            console.log("Money pool for red win: " + result.toNumber());
-            currentMatch.moneyPoolFor = result.toNumber();
-          }).catch(function (e){
-            console.log(e);
-          }); 
-
+          currentMatch = wager.getUpdateMoneyPool(currentMatch);
         }
+
       }
 
     } catch(error) {
